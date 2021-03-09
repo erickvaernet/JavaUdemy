7599cf427c3474de62586bdbed6ab0874d511d9c
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2021-03-09 20:10:09 -0300

    O1-expLamda main2

--- O1ExpLambda/Main2FunctionyPredicate.java
+++ O1ExpLambda/Main2FunctionyPredicate.java
@@ -1,5 +1,7 @@
 package O1ExpLambda;
 
+import java.util.Locale;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Main2FunctionyPredicate {
@@ -10,7 +12,30 @@ public class Main2FunctionyPredicate {
          del valor retornado se declaran en el operadoor <> como <tipoParametro,tipoRetorno>
          */
 
-        Function<String, String> f1= parametro->();
+        Function<String, String> f1= parametro ->"Hola "+parametro;
+
+        String resultado= f1.apply("Erick");
+        System.out.println("resultado = " + resultado);
+
+        //BiFunction es similar, pero recibe 2 paramteros y devuelve un valor
+        BiFunction<String,String,String> concat= (s1,s2)->s1.toUpperCase().concat(s2.toUpperCase());
+        concat.apply("Erick ","Vaernet");
+
+        //Referencia de método
+        Function <String,String> f2=String::toUpperCase;
+        System.out.println(f2.apply("hola"));
+
+        //Lo que se recibe or argumento se usa para invocar algun método, en este caso String
+        BiFunction<String,String,Integer> bf2= String::compareTo;
+        System.out.println(bf2.apply("erick","ian"));
+        BiFunction<String,String,String> bf3= String::concat;
+        System.out.println(bf2.apply("erick","ian"));
+
+        //Predicate
+
+
+
+
 
     }
 
