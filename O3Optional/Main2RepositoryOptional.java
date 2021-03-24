78026cdfe20a10f98c10d28ecacb0748b8a19884
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2021-03-24 08:59:11 -0300

    note1

--- O3Optional/Main2RepositoryOptional.java
+++ O3Optional/Main2RepositoryOptional.java
@@ -3,8 +3,11 @@ package O3Optional;
 import O3Optional.models.Computador;
 import O3Optional.repositorio.ComputadorRepositorio;
 
+import javax.crypto.spec.PSource;
 import javax.swing.*;
+import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main2RepositoryOptional {
     public static void main(String[] args) {
@@ -28,19 +31,28 @@ public class Main2RepositoryOptional {
                 ()-> System.out.println("No se encuentra "+busqueda));
 
         /* podemos usar .orElse, en caso de que no se encuentre el obj se devuelve un obj
-         por defecto indicado en el parametro, DIFERENCIA CON orElseGet() es que or else llama al método del argumetno
-          por defecto, se encuentre o no el objeto, (esto en caso de que se pase por argumento un método que devuelve un obj)
+         por defecto indicado en el parametro, DIFERENCIA CON orElseGet() es que or else crea el objeto si o si,
+         estee presente el objeto principal o no, este objeto por deffeccto se crea si o si
          */
         String busqueda2="sadas";
         Computador defecto= new Computador("Asus 9","x100");
+
         Computador compOpt =repo.filtrar(busqueda2).orElse(defecto);
         System.out.println("compOpt = " + compOpt);
 
-        //orElseGet() a diferencia de solo orElse, usa como parametro un Supplier
-        Computador computadorOpt2= repo.filtrar(busqueda2).orElseGet(()->return defecto);
+        //orElseGet() a diferencia de solo orElse, usa como parametro un Supplier. Mucho mejor que el orElse. Es mas optimizado
+        Computador computadorOpt2 = repo.filtrar(busqueda2).orElseGet(() -> {
+            return defecto;
+        });
 
+        //orElseThrow() En caso de que no exista el objeto y queremos que se lance una excepcion para manejarla.
+        //Computador c3= repo.filtrar(busqueda2).orElseThrow(IllegalStateException::new); // reemplazara al get(?
 
-
+        //ejemplo de busqueda nombre de un archivo
+        String nombreArch= "document.pdf";
+        String opArch= Optional.ofNullable(nombreArch).filter((a)->a.contains("."))
+                .map((a)-> a.substring(a.indexOf(".")+1)).orElseThrow();
+        System.out.println("opArch = " + opArch);
 
     }
 }
