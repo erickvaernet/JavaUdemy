9a5a9dae5d08dc17f02e350acff190d8cda28a76
erick <erickadrielnet@gmail.com>
2021-04-04 19:06:28 -0300

    P3 Composite intermedio

--- P3PatronesDiseno/Composite/Main.java
+++ P3PatronesDiseno/Composite/Main.java
@@ -2,7 +2,11 @@ package P3PatronesDiseno.Composite;
 
 public class Main {
     /*
-    Composite:
+    *Composite: Nos permite construir objetos completos a partir de objetos mas simples que sean similares o del mismo
+    tipo. Nos permite componer objetos en estructuras de árbol y trabajar con esas estructuras como si fueran objetos
+    individuales.
+
+
 
      */
 }
